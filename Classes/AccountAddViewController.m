51cbff68d93aed3231aab9b70126ba9008ec2a4f
David Asabina <david@supr.nu>
2013-07-11 11:47:15 +0200

    Removing host fields and treat https://project.memocom.com as host

--- Classes/AccountAddViewController.m
+++ Classes/AccountAddViewController.m
@@ -12,7 +12,6 @@
 
 @synthesize loginField=_loginField;
 @synthesize passwordField=_passwordField;
-@synthesize urlField=_urlField;
 
 #pragma mark -
 #pragma mark View lifecycle
@@ -37,10 +36,7 @@
 	UIBarButtonItem * doneButton = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(done:)] autorelease];
 	[[self navigationItem] setRightBarButtonItem:doneButton];
 	
-	NSURL * url = [NSURL URLWithString:[[self query] objectForKey:@"url"] ];
-	[_urlField setText:[url absoluteString]];
-
-	Account * account = [Account accountWithURL:[url absoluteString]];
+	Account * account = [Account accountWithURL:@"https://project.memocom.com"];
 	[_loginField	setText:[account username]];
 	[_passwordField setText:[account password]];	
 }
@@ -48,7 +44,6 @@
 - (void)viewWillAppear:(BOOL)animated
 {
 	[super viewWillAppear:animated];
-	[_urlField becomeFirstResponder];
 }
 
 #pragma mark -
@@ -57,9 +52,7 @@
 - (void)dealloc {
 	[_loginField setDelegate:nil];
 	[_passwordField setDelegate:nil];
-	[_urlField setDelegate:nil];
 	
-	TT_RELEASE_SAFELY(_loginField);
 	TT_RELEASE_SAFELY(_passwordField);
 	TT_RELEASE_SAFELY(_urlField);
     [super dealloc];
@@ -69,10 +62,7 @@
 #pragma mark Text field delegate
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-	if (textField == _urlField) {
-		[textField resignFirstResponder];
-		[_loginField becomeFirstResponder];
-	} else if (textField == _loginField) {
+	if (textField == _loginField) {
 	    [textField resignFirstResponder];
 		[_passwordField becomeFirstResponder];
 	} else if (textField == _passwordField) {
@@ -87,15 +77,6 @@
 
 - (IBAction)done:(id)sender {	
 	NSURL * url = [NSURL URLWithString:[_urlField text]];
-	
-	if (!TTIsStringWithAnyText([_urlField text]) || !url) {
-		[_urlField becomeFirstResponder];
-		return [[[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Input error",@"") 
-											message:NSLocalizedString(@"Please enter a valid host",@"") 
-										   delegate:nil 
-								  cancelButtonTitle:TTLocalizedString(@"OK",@"") 
-								  otherButtonTitles:nil] autorelease] show];
-	}
 	
 	NSArray * accounts = [[NSUserDefaults standardUserDefaults] arrayForKey:@"accounts"];
 	if ([accounts containsObject:[url stringByResolvingPathAndRemoveAuthentication]]) {
@@ -116,7 +97,7 @@
 }
 
 - (IBAction)save:(id)sender {
-	NSURL * url = [NSURL URLWithString:[_urlField text]];
+	NSURL * url = [NSURL URLWithString:@"https://project.memocom.com"];
 	Account * account = [Account accountWithURL:[url stringByResolvingPathAndRemoveAuthentication] username:[_loginField text] password:[_passwordField text]];
 	[account save];
 	[self cancel:sender];
